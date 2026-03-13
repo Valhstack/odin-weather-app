97ed8d532cfc34e2b7e9dfdb2c12767fcaea4c1a
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2026-03-13 22:57:59 +0100

    Fixing mixed active content error

--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -2,7 +2,7 @@ import { API_KEY } from "../../config.js";
 import { renderWeatherInfo } from "./render.js";
 
 function fetchWeather(lat, lon, units) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&limit=5&appid=${API_KEY}&units=${units}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&limit=5&appid=${API_KEY}&units=${units}`)
         .then(function (response) {
             return response.json();
         })
